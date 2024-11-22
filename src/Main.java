4d0cd0e0018b326566f6f50f87d7f443e9e06d8f
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-11-22 08:55:45 -0500

    1

--- src/Main.java
+++ src/Main.java
@@ -11,30 +11,91 @@ import java.util.Scanner;
 
         clearBoard(gameBoard);
 
-        InputHelper.getRangedInt(scan, "Enter your move row [1-3]", 1, 3);
+        int x = InputHelper.getRangedInt(scan, "Enter your move row [1-3]", 1, 3);
 
-        InputHelper.getRangedInt(scan, "Enter your move column [1-3]", 1, 3);
+        int y = InputHelper.getRangedInt(scan, "Enter your move column [1-3]", 1, 3);
 
-
+        isValidMove(x, y);
 
     }
 
      private static boolean isValidMove(int row, int col) {
-        String move;
-        boolean matches;
-        for(int r = 0; r < row - 1; r++) {
-            for(int c = 0; c < col - 1; c++) {
-                gameBoard.
-            }
+        boolean valid = false;
+        if (gameBoard[row][col].equalsIgnoreCase("-")) {
+            valid = true;
         }
-     }
+        else {
+            valid = false;
+        }
+        return valid;
+    }
 
 
      private static void clearBoard(String board[][]) {
-        for(int r = 0; r < 3; r++) {
-            for(int c = 0; c < 3; c++) {
-                System.out.println(board[r][c] + "-");
+        for(int r = 0; r < gameBoard.length; r++) {
+            for(int c = 0; c < gameBoard[0].length; c++) {
+                board[r][c] = " - ";
             }
         }
     }
-}
+
+ private static void displayBoard(String board[][]) {
+     for(int r = 0; r < gameBoard.length; r++) {
+         for(int c = 0; c < gameBoard[0].length; c++) {
+             System.out.print(board[r][c]);
+         }
+         System.out.println();
+     }
+ }
+
+ private static boolean isWin(String player) {
+
+ }
+
+ private static boolean isColWin(String player) {
+    boolean matches = true;
+        for(int c = 0; c < gameBoard[0].length; c++) {
+            matches = true;
+                for(int r = 0; r < gameBoard.length; c++) {
+                    if(!player.equals(gameBoard[r][c])) {
+                        matches = false;
+                        break;
+                    }
+                }
+               if (matches) {
+                   break;
+               }
+        }
+        return matches;
+ }
+
+ private static boolean isRowWin(String player) {
+     boolean matches = true;
+     for(int r = 0; r < gameBoard[0].length; r++) {
+         matches = true;
+         for(int c = 0; c < gameBoard.length; c++) {
+             if(!player.equals(gameBoard[r][c])) {
+                 matches = false;
+                 break;
+             }
+         }
+         if (matches) {
+             break;
+         }
+     }
+     return matches;
+ }
+
+ private static boolean isDiagonalWin(String player) {
+
+ }
+
+ private static boolean isTie() {
+
+ }
+
+
+
+
+
+ }
